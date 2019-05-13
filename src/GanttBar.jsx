b76c9ea45c5c735b72b732829f8390d2c0ea59e4
Raphael Montani <raphael.muntani@gmail.com>
2019-05-12 22:44:54 -0300

    Bar hovering

--- src/GanttBar.jsx
+++ src/GanttBar.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { config } from './gantt.config';
 
 class GanttBar extends Component {
   constructor(props) {
     super(props);
+    this.duration = props.duration;
+    this.externalStyle = props.style;
+    this.id = props.id;
+    this.mouseOver = this.mouseOver.bind(this);
+    this.mouseLeave = this.mouseLeave.bind(this);
+    this.myRef = React.createRef();
+    this.onDoubleClick = props.onDoubleClick;
     this.scale = props.scale;
     this.start = props.start;
-    this.id = props.id;
-    this.externalStyle = props.style;
-    this.onDoubleClick = props.onDoubleClick;
-    this.duration = props.duration;
+    this.state = {
+      showDetails: false,
+    };
   }
 
   shouldComponentUpdate(nextProps) {
@@ -41,15 +48,53 @@ class GanttBar extends Component {
     return `${this.duration / this.scale * 100}%`;
   }
 
+  getDetails() {
+    const { showDetails, x, y } = this.state;
+    const detailsLeftPosition = x + this.duration / this.scale * config.chartWidth;
+    if (showDetails) {
+      return (
+        <div className="details" style={{ left: detailsLeftPosition, top: y }}>
+          <p>
+            Start:
+            {' '}
+            <small>{this.start}</small>
+          </p>
+          <p>
+            Duration:
+            {' '}
+            <small>{this.duration}</small>
+          </p>
+        </div>
+      );
+    }
+
+    return null;
+  }
+
+  mouseOver() {
+    const x = this.myRef.current.offsetLeft;
+    const y = this.myRef.current.offsetTop;
+    this.setState(() => ({ showDetails: true, x, y }));
+  }
+
+  mouseLeave() {
+    this.setState(() => ({ showDetails: false }));
+  }
 
   render() {
     const barId = this.id;
+
     return (
       <div
-        style={this.getStyle()}
-        onDoubleClick={() => this.onDoubleClick(barId)}
         className="bar"
-      />
+        onMouseOver={this.mouseOver}
+        onMouseLeave={this.mouseLeave}
+        onDoubleClick={() => this.onDoubleClick(barId)}
+        style={this.getStyle()}
+        ref={this.myRef}
+      >
+        {this.getDetails()}
+      </div>
     );
   }
 }
--- tests/GanttBar.test.jsx
+++ tests/GanttBar.test.jsx
@@ -1,17 +1,45 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 import GanttBar from '../src/GanttBar';
 
 describe('GanttBar', () => {
   it('can render a bar with the correct margin left', () => {
-    const wrapper = shallow(<GanttBar scale={100} duration={20} start={30} />);
-    const { style } = wrapper.get(0).props;
+    const wrapper = mount(<GanttBar scale={100} duration={20} start={30} />);
+    const { style } = wrapper.childAt(0).props();
     expect(style.marginLeft).toEqual('30%');
   });
 
   it('can render a bar with the correct width', () => {
-    const wrapper = shallow(<GanttBar scale={100} duration={20} start={30} />);
-    const { style } = wrapper.get(0).props;
+    const wrapper = mount(<GanttBar scale={100} duration={20} start={30} />);
+    const { style } = wrapper.childAt(0).props();
     expect(style.width).toEqual('20%');
+  });
+
+  it('shows tasks details when bar is hovered', () => {
+    const wrapper = mount(<GanttBar scale={100} duration={20} start={30} />);
+    const bar = wrapper.find('div.bar');
+
+    bar.prop('onMouseOver')();
+    wrapper.update();
+
+    const details = wrapper.find('div.details');
+    const detailsInfos = details.find('p');
+    const detailsTexts = detailsInfos.map(detail => detail.text());
+
+    expect(details.exists()).toEqual(true);
+    expect(detailsInfos.length).toEqual(2);
+    expect(detailsTexts).toEqual(expect.arrayContaining(['Start: 30', 'Duration: 20']));
+  });
+
+  it('removes task details when mouse leaves bar', () => {
+    const wrapper = mount(<GanttBar scale={100} duration={20} start={30} />);
+    const bar = wrapper.find('div.bar');
+
+    bar.prop('onMouseOver')();
+    wrapper.update();
+    bar.prop('onMouseLeave')();
+    wrapper.update();
+
+    expect(wrapper.find('div.details').exists()).toEqual(false);
   });
 });
